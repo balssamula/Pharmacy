d5ca64fa39a1669844c7e46cb4b5463d9cae27fc
balssamula <balssamula@gmail.com>
2026-06-29 17:37:45 +0300

    Update products_page.py

--- products_page.py
+++ products_page.py
@@ -565,8 +565,8 @@ def render_products_page():
         p_image = p.get('thumbnail') or p.get('main_image')
         
         promo = p.get('promotion', {})
-        p_promotion = p.get('promotion_title') or (promo.get('title') if isinstance(promo, dict) else '') or "لا يوجد عنوان ترويجي"
-        p_sub_title = (promo.get('sub_title') if isinstance(promo, dict) else '') or "لا يوجد عنوان فرعي"
+        p_promotion = p.get('promotion_title') or (promo.get('title') if isinstance(promo, dict) else '') or "-"
+        p_sub_title = (promo.get('sub_title') if isinstance(promo, dict) else '') or "-"
         
         price_val = get_flat_price(p.get('price', 0))
         regular_val = get_flat_price(p.get('regular_price', 0))
